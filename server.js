1c0896b5fb113e02c6fbaa135c94c824d5e5d15e
jbus97 <hugofleitasbus@gmail.com>
2021-05-03 17:15:34 -0400

    Inicia el proyecto

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+    origin: "http://localhost:9091"
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.json({message: "Hola"});
+});
+
+const PORT = process.env.PORT || 9090;
+app.listen(PORT,()=>{
+    console.log("Servidor corriendo en el puerto 9090");
+});

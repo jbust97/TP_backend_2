77204fba0e0e17712bfcf32c452fb0c02059cda3
jbus97 <hugofleitasbus@gmail.com>
2021-05-04 10:01:47 -0400

    Minor fix llamada a json()) ¨

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var corsOptions = {
     origin: "http://localhost:9091"
 }
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
+app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
